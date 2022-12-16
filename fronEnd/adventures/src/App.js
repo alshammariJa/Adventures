df89a960b7452757ae399e2fe06f4a78082e564b
Jamilah Alshammari <114332989+alshammariJa@users.noreply.github.com>
2022-12-16 23:02:08 +0300

    new project

---
+++ fronEnd/adventures/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import SignOut from ".SignOut";
+import SignIn from "./SignIn"
+ import UpdateUser from "./UpdateUser"
+function App() {
+  return (
+    <div >
+    {/* <SignOut/>   */}
+    {/* <SignIn/>    */}
+    <UpdateUser/> 
+   
+    </div>
+  );
+}
+
+export default App;
